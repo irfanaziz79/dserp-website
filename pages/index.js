3882e21b7d99b4f4c1b61cc2c23e944e03e1c711
irfanaziz79 <Irfanmaziz@gmail.com>
2026-06-02 02:13:34 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', fontFamily: 'Arial', textAlign: 'center' }}>
-      <h1>SAP FI/CO Close Acceleration</h1>
-      <p>Website coming soon...</p>
-      <p>Your diagnostic tool is ready to launch.</p>
-    </div>
-  )
-}
